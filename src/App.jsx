99c21c603f4e97f333f242b5cfaec5d2cdcce635
AqibAhmed885 <aqibahmed885@gmail.com>
2024-12-05 00:30:07 +0500

    react task is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-
+import { useEffect } from "react";
+import { useState } from "react"
+import Card from "./components/shared/Card";
+import ImageSearch from "./components/shared/ImageSearch";
+// Remove dotenv import and config as it's not needed in create-react-app
+// Remove dotenv import and config as it's not needed in create-react-app
 
 function App() {
 
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    // fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API}&q=${term}&image_type=photo&pretty=true`)
+    const apiKey = import.meta.env.VITE_PIXABAY_API_KEY;
+    fetch(`https://pixabay.com/api/?key=${apiKey}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Image Gallery
-    </h1>
-    </>
+    <div className="container mx-auto mt-10">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <h1>Loading...</h1>
+        </div>
+
+      ) : (
+        <div className="grid grid-cols-2 gap-4">
+          {images.map(image => (
+            <Card key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+
   )
 }
 
